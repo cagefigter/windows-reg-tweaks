2817ebbc4225f30b9e6e488050dfc2e12ecee516
CHEF-KOCH <Nvinsidemail.com>
2015-10-09 01:09:31 +0200

    add firewall script to fix webrtc on per firewall basis (or just use uBlock)
    Signed-off-by: CHEF-KOCH <Nvinsidemail.com>

---
+++ All/test/WEBRTC browser IP leak fix/webrtc_firewall.bat
@@ -0,0 +1,14 @@
+@echo off
+::Run this as administrator
+::then visit https://diafygi.github.io/webrtc-ips/ to verify
+::no more public IP leaking :-D
+:: Original by https://github.com/stunner all credits goes to him 
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP localport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP remoteport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP localport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP remoteport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP localport=19302
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP remoteport=19302
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP localport=19302
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP remoteport=19302
+
