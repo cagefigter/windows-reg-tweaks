6c7187b9bbbfd868d94ef166f65b30b720f53f67
CHEF-KOCH <Nvinsidemail.com>
2015-10-12 02:27:46 +0200

    missing stuff added
    Signed-off-by: CHEF-KOCH <Nvinsidemail.com>

--- Win 10/Remove Adobe Flash/Remove Adobe Flash.bat
+++ Win 10/Remove Adobe Flash/Remove Adobe Flash.bat
@@ -9,6 +9,13 @@ echo Run this script as admin.
 echo.
 pause
 
+::# Take ownership
+takeown /f C:\Windows\SysWOW64\Macromed\Flash\*.*
+takeown /f C:\Windows\System32\Macromed\Flash\*.*
+
+::cacls C:\Windows\SysWOW64\Macromed\Flash\*.* /E /T /G %UserDomain%\%UserName%:F
+::cacls C:\Windows\System32\Macromed\Flash\*.* /E /T /G %UserDomain%\%UserName%:F
+
 takeown /f "%windir%\System32\Macromed" /r /d y
 icacls "%windir%\System32\Macromed" /grant administrators:F /t
 rd /s /q "%windir%\System32\Macromed"
@@ -19,8 +26,27 @@ icacls "%windir%\SysWOW64\Macromed" /grant administrators:F /t
 rd /s /q "%windir%\SysWOW64\Macromed"
 echo.
 
+takeown /f "%windir%\SysWOW64\FlashPlayerApp.exe" /r /d y
+icacls "%windir%\SysWOW64\FlashPlayerApp.exe" /grant administrators:F /t
+rd /s /q "%windir%\SysWOW64\FlashPlayerApp.exe"
+echo.
+
+takeown /f "%windir%\SysWOW64\FlashPlayerCPLApp.cpl" /r /d y
+icacls "%windir%\SysWOW64\FlashPlayerCPLApp.cpl" /grant administrators:F /t
+rd /s /q "%windir%\SysWOW64\FlashPlayerCPLApp.cpl"
+echo.
+
 rd /s /q "%appdata%\Adobe"
 rd /s /q "%appdata%\Macromedia"
 echo.
+
+::del C:\Windows\SysWOW64\Macromed\Flash\*.* /Q
+::del C:\Windows\System32\Macromed\Flash\*.* /Q
+::rd C:\Windows\SysWOW64\Macromed\Flash
+::rd C:\Windows\System32\Macromed\Flash
+::rd C:\Windows\SysWOW64\Macromed
+::rd C:\Windows\System32\Macromed
+::del C:\Windows\SysWOW64\FlashPlayerApp.exe
+::del C:\Windows\SysWOW64\FlashPlayerCPLApp.cpl
 
 pause
