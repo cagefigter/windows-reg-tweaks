6f933af25d90e515a4361c44d4bb38a92cdc6361
CHEF-KOCH <Nvinside@gmail.com>
2016-12-18 16:38:02 +0100

    some new stuff added
    Signed-off-by: CHEF-KOCH <Nvinside@gmail.com>

---
+++ Win 10/gpedit/gpedit.msc for home.bat
@@ -0,0 +1,10 @@
+@echo off 
+
+:: Run as Admin + Reboot PC after apply.
+
+pushd "%~dp0" 
+dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum >List.txt 
+dir /b C:\Windows\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum >>List.txt 
+
+for /f %%i in ('findstr /i . List.txt 2^>nul') do dism /online /norestart /add-package:"C:\Windows\servicing\Packages\%%i" 
+pause
