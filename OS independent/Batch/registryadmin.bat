f559ca8fdb515a2012b529016068507797e4b723
CHEF-KOCH <Nvinside@gmail.com>
2017-01-26 12:10:06 +0100

    useless stuff removed, comments added
    Signed-off-by: CHEF-KOCH <Nvinside@gmail.com>

--- OS independent/Batch/registryadmin.bat
+++ OS independent/Batch/registryadmin.bat
@@ -1,5 +1,6 @@
 @echo off
  
+::Get full access to all Registry related sub-keys
 subinacl /subkeyreg HKEY_LOCAL_MACHINE /grant=administrators=f
 subinacl /subkeyreg HKEY_CURRENT_USER /grant=administrators=f
 subinacl /subkeyreg HKEY_CLASSES_ROOT /grant=administrators=f
--- OS independent/Batch/remove_removal_tools.bat
+++
@@ -1,2 +0,0 @@
-reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\MRT /v DontOfferThroughWUAU /t REG_DWORD /d 1
-wusa /uninstall /kb:890830 /norestart 
