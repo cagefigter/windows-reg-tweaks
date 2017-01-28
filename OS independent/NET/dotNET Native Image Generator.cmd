871a7a6d1b90723327e995f34d4e4b2247b0abeb
CHEF-KOCH <Nvinside@gmail.com>
2017-01-28 03:31:57 +0100

    some new stuff added
    Signed-off-by: CHEF-KOCH <Nvinside@gmail.com>

---
+++ OS independent/NET/dotNET Native Image Generator.cmd
@@ -0,0 +1,6 @@
+"%windir%\Microsoft.NET\Framework\v2.0.50727\ngen.exe" update /force
+"%windir%\Microsoft.NET\Framework64\v2.0.50727\ngen.exe" update /force
+"%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe" update /force
+"%windir%\Microsoft.NET\Framework\v4.0.30319\ngentask.exe" /runtimewide
+"%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe" update /force
+"%windir%\Microsoft.NET\Framework64\v4.0.30319\ngentask.exe" /runtimewide
