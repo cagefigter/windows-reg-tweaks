ac1f1b691bfdd5a3c3e817633f8ec46d6ed4ab4a
CHEF-KOCH <Nvinside@gmail.com>
2017-06-23 11:30:24 +0200

    powershell script added to disable port 445
    Signed-off-by: CHEF-KOCH <Nvinside@gmail.com>

---
+++ OS independent/Security/Ports/Disable445Feature.ps1
@@ -0,0 +1,12 @@
+﻿Function Disable445Feature
+{
+    $netBTParametersPath = "HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters"
+    IF(Test-Path -Path $netBTParametersPath) {
+        Set-ItemProperty -Path $netBTParametersPath -Name "SMBDeviceEnabled" -Value 0
+    }
+    Set-Service lanmanserver -StartupType Disabled
+    Stop-Service lanmanserver -Force
+}
+
+Disable445Feature
+
