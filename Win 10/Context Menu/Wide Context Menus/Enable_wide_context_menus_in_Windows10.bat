90915dc3585838c5be8cedc6ae1e8c62947b39c3
CHEF-KOCH <Nvinside@gmail.com>
2017-07-26 02:18:32 +0200

    enable/disable wide context menu
    Signed-off-by: CHEF-KOCH <Nvinside@gmail.com>

---
+++ Win 10/Context Menu/Wide Context Menus/Enable_wide_context_menus_in_Windows10.bat
@@ -0,0 +1,5 @@
+@echo off
+powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\FlightedFeatures" /V ImmersiveContextMenu /T REG_DWORD /D 1 /F' -Verb runAs"
+
+taskkill /f /im explorer.exe
+start explorer.exe
