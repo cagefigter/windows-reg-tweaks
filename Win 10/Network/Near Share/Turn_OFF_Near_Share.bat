8d50f3aa2d298ff0c8a074f14c7a1681bd413847
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2017-11-14 02:12:10 +0100

    Turn on/off near sharing device information
    Signed-off-by: CHEF-KOCH <CHEF-KOCH@protonmail.com>

---
+++ Win 10/Network/Near Share/Turn_OFF_Near_Share.bat
@@ -0,0 +1,12 @@
+:: Created by: Shawn Brink
+:: Created on: November 8th 2017
+:: Tutorial: https://www.tenforums.com/tutorials/97582-turn-off-near-share-windows-10-a.html
+
+
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\CDP" /V CdpSessionUserAuthzPolicy /T REG_DWORD /D 0 /F
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\CDP" /V NearShareChannelUserAuthzPolicy /T REG_DWORD /D 0 /F
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\CDP\SettingsPage" /V BluetoothLastDisabledNearShare /T REG_DWORD /D 0 /F
+
+
+taskkill /f /im explorer.exe
+start explorer.exe
