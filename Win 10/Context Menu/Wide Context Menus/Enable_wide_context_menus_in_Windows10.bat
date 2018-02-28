0243d27a81cd4412b2afb63929e76f9f151ff4e2
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2018-03-01 00:12:40 +0100

    New structure changes, some 'tweaks'/options are Redstone X exclusive, which means you need a specific Windows version 1703,106,1709,1803,.. in order to use the mentioned tweak. So let's give them own categories.

--- Win 10/Context Menu/Wide Context Menus/Enable_wide_context_menus_in_Windows10.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\FlightedFeatures" /V ImmersiveContextMenu /T REG_DWORD /D 1 /F' -Verb runAs"
-
-taskkill /f /im explorer.exe
-start explorer.exe
